698fdd6a8bc0cc9de6e984e70bc0e2d06fa3369d
JotaPe <cronos2277@gmail.com>
2019-12-01 18:40:54 -0300

    commit MYSQL

--- mysql/basico.sql
+++ mysql/basico.sql
@@ -52,6 +52,7 @@ create table estados (
     UNIQUE KEY(nome),
     UNIQUE KEY(sigla)    
 );
+
 -- Aqui abaixo temos um exemplo do comando insert into. Sendo a sua sintaxe:
 -- INSERT INTO <TABELA> (CAMPOS) VALUES ('VALORES') => Apenas os valores com aspas, os campos nao.
 -- Voce tambem pode omitir na sintaxe os campos, caso voce coloque todos os valores e 
@@ -64,3 +65,30 @@ insert into estados values (2,'Alagoas','AL','NORDESTE',3.38);
 -- Voce tambem pode colocar mais de um valor por vez, mas use a virgula como separador:
 insert into estados(nome,sigla,regiao,populacao) values 
 ('Amapa','AP','NORTE',0.8),('Amazonas','AM','NORTE',4.06);
+-- Por fim inserindo o restante dos Estados Brasileiros
+INSERT INTO estados
+    (nome, sigla, regiao, populacao)
+VALUES
+    ('Bahia', 'BA', 'Nordeste', 15.34),
+    ('Ceará', 'CE', 'Nordeste', 9.02),
+    ('Distrito Federal', 'DF', 'Centro-Oeste', 3.04),
+    ('Espírito Santo', 'ES', 'Sudeste', 4.02),
+    ('Goiás', 'GO', 'Centro-Oeste', 6.78),
+    ('Maranhao', 'MA', 'Nordeste', 7.00),
+    ('Mato Grosso', 'MT', 'Centro-Oeste', 3.34),
+    ('Mato Grosso do Sul', 'MS', 'Centro-Oeste', 2.71),
+    ('Minas Gerais', 'MG', 'Sudeste', 21.12),
+    ('Pará', 'PA', 'Norte', 8.36),
+    ('Paraíba', 'PB', 'Nordeste', 4.03),
+    ('Parana', 'PR', 'Sul', 11.22),
+    ('Pernambuco', 'PE', 'Nordeste', 9.47),
+    ('Piauí', 'PI', 'Nordeste', 3.22),
+    ('Rio de Janeiro', 'RJ', 'Sudeste', 16.72),
+    ('Rio Grande do Norte', 'RN', 'Nordeste', 3.51),
+    ('Rio Grande do Sul', 'RS', 'Sul', 11.32),
+    ('Rondônia', 'RO', 'Norte', 1.81),
+    ('Roraima', 'RR', 'Norte', 0.52),
+    ('Santa Catarina', 'SC', 'Sul', 7.01),
+    ('São Paulo', 'SP', 'Sudeste', 45.10),
+    ('Sergipe', 'SE', 'Nordeste', 2.29),
+    ('Tocantins', 'TO', 'Norte', 1.55);
